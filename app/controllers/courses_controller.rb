dbdddf549223c6efab1bdc481e3c4b4b2833f7e4
José Tomás González <jtgonzalez@miuandes.cl>
2017-04-02 20:47:03 -0300

    Updated courses controllers

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -5,12 +5,6 @@ class CoursesController < ApplicationController
   # GET /courses.json
   def index
     @courses = Course.all
-    
-    if params[:search]
-      @courses = Course.search(params[:search]).order('created_at DESC')
-    else
-      @courses = Course.all.order('created_at DESC')
-    end
   end
 
   # GET /courses/1
@@ -75,13 +69,6 @@ class CoursesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def course_params
-      teacher_id = params.require(:course)[:teacher]
-      if teacher_id
-        teacher = Person.find(teacher_id)
-      else
-        teacher = nil
-      end
-
-      params.require(:course).permit(:title, :code).merge(:teacher => teacher)
+      params.require(:course).permit(:title, :code, :quote, :person_id)
     end
 end
